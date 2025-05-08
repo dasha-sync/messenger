4191e80e6cc324cbef1de137c6b8accdf58f8809
Darya Kapoor <kapoor.darya@gmail.com>
2025-05-08 23:10:39 +0300

    WIP

--- src/main/java/com/talkwire/messenger/controller/SecurityController.java
+++ src/main/java/com/talkwire/messenger/controller/SecurityController.java
@@ -6,7 +6,7 @@ import com.talkwire.messenger.model.User;
 import com.talkwire.messenger.repository.UserRepository;
 import com.talkwire.messenger.security.JwtTokenProvider;
 import java.security.NoSuchAlgorithmException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,64 +22,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@AllArgsConstructor
 public class SecurityController {
-  private UserRepository userRepository;
-  private PasswordEncoder passwordEncoder;
-  private AuthenticationManager authenticationManager;
-  private JwtTokenProvider jwtTokenProvider;
 
-  @Autowired
-  public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-    this.passwordEncoder = passwordEncoder;
-  }
-
-  @Autowired
-  public void setUserRepository(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
-  @Autowired
-  public void setAuthenticationManager(AuthenticationManager authenticationManager) {
-    this.authenticationManager = authenticationManager;
-  }
-
-  @Autowired
-  public void setJwtCore(JwtTokenProvider jwtTokenProvider) {
-    this.jwtTokenProvider = jwtTokenProvider;
-  }
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManager authenticationManager;
+  private final JwtTokenProvider jwtTokenProvider;
 
   @PostMapping("/signup")
-  ResponseEntity<?> signup(@RequestBody SignupRequest signupDto) {
+  public ResponseEntity<String> signup(@RequestBody SignupRequest signupDto) {
     if (userRepository.existsUserByUsername(signupDto.getUsername())) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Choose different name");
-    }
-    if (userRepository.existsUserByEmail(signupDto.getEmail())) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Choose different email");
+      return ResponseEntity.badRequest().body("Username already taken");
     }
 
-    String hashed = passwordEncoder.encode(signupDto.getPassword());
+    if (userRepository.existsUserByEmail(signupDto.getEmail())) {
+      return ResponseEntity.badRequest().body("Email already registered");
+    }
+
     User user = new User();
     user.setUsername(signupDto.getUsername());
     user.setEmail(signupDto.getEmail());
-    user.setPassword(hashed);
+    user.setPassword(passwordEncoder.encode(signupDto.getPassword()));
     userRepository.save(user);
 
-    return ResponseEntity.ok("Signup successfully completed");
+    return ResponseEntity.ok("Signup successful");
   }
 
   @PostMapping("/signin")
-  ResponseEntity<?> signin(@RequestBody SigninRequest singinDto) throws NoSuchAlgorithmException {
-    Authentication authentication = null;
+  public ResponseEntity<?> signin(@RequestBody SigninRequest signinDto) {
     try {
-      authentication = authenticationManager
-              .authenticate(new UsernamePasswordAuthenticationToken(
-                      singinDto.getUsername(), singinDto.getPassword()));
-    } catch (BadCredentialsException e) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+      Authentication authentication = authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+              signinDto.getUsername(), signinDto.getPassword())
+      );
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      String jwt = jwtTokenProvider.generateToken(authentication);
+
+      return ResponseEntity.ok(jwt);
+    } catch (BadCredentialsException | NoSuchAlgorithmException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
     }
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtTokenProvider.generateToken(authentication);
-    
-    return ResponseEntity.ok(jwt);
   }
 }
