c6d0ff303b3bd43f74344337700f23227b29578e
Darya Kapoor <kapoor.darya@gmail.com>
2025-05-10 23:15:12 +0300

    add user controller

--- src/main/java/com/talkwire/messenger/repository/UserRepository.java
+++ src/main/java/com/talkwire/messenger/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.talkwire.messenger.repository;
 
 import com.talkwire.messenger.model.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Boolean existsUserByUsername(String username);
 
   Boolean existsUserByEmail(String email);
+
+  List<User> findAllByOrderByUsernameAsc();
+
+  List<User> findByUsernameContainingIgnoreCaseAndEmailContainingIgnoreCase(String username, String email);
+
 }
