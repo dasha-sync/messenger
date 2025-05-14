5c4e4ba6d89d2d096e51e743d44c1c6d4ea765f1
Darya Kapoor <kapoor.darya@gmail.com>
2025-05-14 20:06:13 +0300

    cover some behavioral scripts

--- src/main/java/com/talkwire/messenger/exception/GlobalExceptionHandler.java
+++ src/main/java/com/talkwire/messenger/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(ResponseStatusException.class)
