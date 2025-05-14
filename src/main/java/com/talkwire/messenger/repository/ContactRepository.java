77cf6a322f521969e5b39e60ea7ba6357a74ad63
daria <kapoor.darya@gmai.com>
2025-05-14 19:50:07 +0300

    add contact controller

--- src/main/java/com/talkwire/messenger/repository/ContactRepository.java
+++ src/main/java/com/talkwire/messenger/repository/ContactRepository.java
@@ -1,9 +1,11 @@
 package com.talkwire.messenger.repository;
 
 import com.talkwire.messenger.model.Contact;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+  List<Contact> findAllByUserId(Long userId);
 }
