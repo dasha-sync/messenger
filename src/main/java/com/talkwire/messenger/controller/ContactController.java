16ca6f1d2b7749a00326ddd5412271d3b2425b97
daria <kapoor.darya@gmai.com>
2025-05-14 19:50:07 +0300

    add contact controller

--- src/main/java/com/talkwire/messenger/controller/ContactController.java
+++ src/main/java/com/talkwire/messenger/controller/ContactController.java
@@ -1,5 +1,37 @@
 package com.talkwire.messenger.controller;
 
-public class ContactController {
+import com.talkwire.messenger.dto.common.ApiResponse;
+import com.talkwire.messenger.dto.contact.ContactResponse;
+import com.talkwire.messenger.service.ContactService;
+import java.security.Principal;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/secured/contacts")
+@PreAuthorize("isAuthenticated()")
+@RequiredArgsConstructor
+public class ContactController {
+  private final ContactService contactService;
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<ContactResponse>>> getContacts(Principal principal) {
+    List<ContactResponse> response = contactService.getContacts(principal);
+    return ResponseEntity.ok(new ApiResponse<>("Contacts retrieved successfully", response));
+  }
+
+  @DeleteMapping("/{contactId}/destroy")
+  public ResponseEntity<ApiResponse<Void>> deleteUserRequest(
+      @PathVariable Long contactId,
+      Principal principal) {
+    contactService.deleteContact(contactId, principal);
+    return ResponseEntity.ok(new ApiResponse<>("Contact deleted successfully", null));
+  }
 }
--- src/main/java/com/talkwire/messenger/service/ContactService.java
+++ src/main/java/com/talkwire/messenger/service/ContactService.java
@@ -2,12 +2,37 @@ package com.talkwire.messenger.service;
 
 import com.talkwire.messenger.dto.contact.ContactResponse;
 import com.talkwire.messenger.model.Contact;
+import com.talkwire.messenger.model.User;
+import com.talkwire.messenger.repository.ContactRepository;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ContactService {
+  private final UserService userService;
+  private final ContactRepository contactRepository;
+
+  public List<ContactResponse> getContacts(Principal principal) {
+    User currentUser = userService.getCurrentUser(principal);
+    return contactRepository.findAllByUserId(currentUser.getId())
+        .stream()
+        .map(this::mapToContactDto)
+        .toList();
+  }
+
+  public void deleteContact(Long contactId, Principal principal) {
+    User currentUser = userService.getCurrentUser(principal);
+    // TODO: ContactNotFoundException
+    Contact contact = contactRepository.findById(contactId)
+        .orElseThrow(() -> new RuntimeException("Contact not found"));
+
+    validateContactAccess(contact, currentUser.getId());
+    contactRepository.delete(contact);
+  }
+
   public ContactResponse mapToContactDto(Contact contact) {
     return new ContactResponse(
         contact.getId(),
@@ -15,5 +40,12 @@ public class ContactService {
         contact.getUser().getUsername(),
         contact.getContact().getId(),
         contact.getContact().getUsername());
+  }
+
+  // TODO: ContactAccessDeniedException
+  private void validateContactAccess(Contact contact, Long userId) {
+    if (!contact.getUser().getId().equals(userId)) {
+      throw new RuntimeException("Access denied: It is not your request");
+    }
   }
 }
